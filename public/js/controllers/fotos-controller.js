40810fc1fbd61a415bc3f5d1fcbce4f7f80b7fee
dpaula <fernando.dpaula@gmail.com>
2018-06-09 14:16:25 -0300

    usando diretiva ng-repeat para iterar sobre as fotos

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,17 +1,17 @@
 //usando modulo alurapic, criamos um controller com uma função, para prover o model a view
 angular.module('alurapic').controller('FotosController', function($scope){//injeção de dependencia, pre requisito $scope para definição do escopo 
 
-    $scope.foto = {
-        titulo : 'Leão',
+    $scope.fotos = [
+        {
+        titulo : 'Leão 1',
         url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-    };
-    $scope.foto2 = {
+        },{
         titulo : 'Leão 2',
         url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-    };
-    $scope.foto3 = {
+        },{
         titulo : 'Leão 3',
         url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-    };
-
+        }
+    ];
+ 
 });
