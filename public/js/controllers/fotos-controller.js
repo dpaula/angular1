0dbdd7f27bfe1815141e0717636074644083464c
dpaula <fernando.dpaula@gmail.com>
2018-06-09 13:27:57 -0300

    injeção de dependencia, $scope para fornecer pojo para view

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,7 +1,7 @@
 //usando modulo alurapic, criamos um controller com uma função, para prover o model a view
-angular.module('alurapic').controller('FotosController', function(){
+angular.module('alurapic').controller('FotosController', function($scope){//injeção de dependencia, pre requisito $scope para definição do escopo 
 
-    let foto = {
+    $scope.foto = {
         titulo : 'Leão',
         url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
     };
