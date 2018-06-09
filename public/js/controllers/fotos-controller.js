32a2f594427b537c51d9e94f5be4e233b126b21a
dpaula <fernando.dpaula@gmail.com>
2018-06-09 14:05:28 -0300

    novas divs bootstrap e novas imagens na view

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -5,5 +5,13 @@ angular.module('alurapic').controller('FotosController', function($scope){//inje
         titulo : 'Leão',
         url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
     };
+    $scope.foto2 = {
+        titulo : 'Leão 2',
+        url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
+    };
+    $scope.foto3 = {
+        titulo : 'Leão 3',
+        url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
+    };
 
 });
