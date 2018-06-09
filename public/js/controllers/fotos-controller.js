033a2826a7b93ff8c8d2968bd077f4b0981056aa
dpaula <fernando.dpaula@gmail.com>
2018-06-09 14:32:24 -0300

    requisiçao ajax, usando $http do angular para trazer fotos do server

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,17 +1,15 @@
 //usando modulo alurapic, criamos um controller com uma função, para prover o model a view
-angular.module('alurapic').controller('FotosController', function($scope){//injeção de dependencia, pre requisito $scope para definição do escopo 
+angular.module('alurapic').controller('FotosController', function($scope, $http){//injeção de dependencia, pre requisito $scope para definição do escopo 
 
-    $scope.fotos = [
-        {
-        titulo : 'Leão 1',
-        url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-        },{
-        titulo : 'Leão 2',
-        url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-        },{
-        titulo : 'Leão 3',
-        url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-        }
-    ];
+    $scope.fotos = [];
+
+    //requisição ajax, trazendo do servidor as fotos, usando $http do angular
+    let promisse = $http.get('v1/fotos');
+
+    promisse.then((ret) => {
+        $scope.fotos = ret.data;
+    }).catch((error) => {
+        console.log(error);
+    });
  
 });
