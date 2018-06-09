3b201924e446c51d5e60dd74adf200ed0df221a7
dpaula <fernando.dpaula@gmail.com>
2018-06-09 13:18:36 -0300

    criação do controller ffotos-controller para prover o model foto a view

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,9 @@
+//usando modulo alurapic, criamos um controller com uma função, para prover o model a view
+angular.module('alurapic').controller('FotosController', function(){
+
+    let foto = {
+        titulo : 'Leão',
+        url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
+    };
+
+});
