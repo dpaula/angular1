f1494c7d94f80e62d48a2fd74a1df6a2d9e37518
dpaula <fernando.dpaula@gmail.com>
2018-06-10 12:53:11 -0300

    incluida nova diretiva customizada

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -1,1 +1,21 @@
-angular.module('minhasDiretivas', []);
+angular.module('minhasDiretivas', [])
+.directive('meuPainel', () =>{
+
+    let ddo = {};
+
+    //definindo que a diretiva será usada como Atributo e Element
+    ddo.restrict = "AE";
+
+    //escopo de atributos 
+    ddo.scope = {
+        titulo : '@' //interface de comunicação com a diretiva, como passa o dado para dentro do escopo da diretiva
+    };
+
+    ddo.transclude = true; //para diretiva manter elementos filhos quando angular for processar o html
+
+    //apontando para o html com as diretivas novas
+    ddo.templateUrl = 'js/directives/meu-painel.html';
+
+    return ddo;
+
+});
