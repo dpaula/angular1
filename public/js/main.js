3d78cde94f757b7e1103bb5cdd0c8df112b02b29
dpaula <fernando.dpaula@gmail.com>
2018-06-11 21:57:43 -0300

    configurando rota para fotos

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,11 @@
-angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute']);
+angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
+.config(function($routeProvider) {// não funciona com arrow function
+
+    //configuração de rotas
+    //primeiro parâmetro é a rota indicada /#/fotos
+    $routeProvider.when('/fotos', {//segundo parametro é um objeto
+        templateUrl : 'partials/principal.html',//url do html que será monntado
+        controller : 'FotosController'//controller que será associado
+    });
+
+});
