6b4100a5295ae547719c468f1e21fc54d2ca7bd8
dpaula <fernando.dpaula@gmail.com>
2018-06-16 20:55:50 -0300

    configurado controller para enviar fotos para o back end pelo metodo post

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,9 +1,15 @@
-angular.module('alurapic').controller('FotoController', function($scope) {
+angular.module('alurapic').controller('FotoController', function ($scope, $http) {
 
     $scope.foto = {};
 
-    $scope.submeter = () => {
-        console.log($scope.foto);
-    }
+    $scope.submeter = () => {//método chamado na tag do formulário da foto
+        $http.post("v1/fotos", $scope.foto) //envia foto para backend
+            .success(() => {
+                alert("Foto cadastrada com sucesso!");
+            })
+            .error((error) => {
+                console.log(error);
+            });
+    };
 
 });
