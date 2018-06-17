ad03ea7bb89567b7ef979afd48b102ffef1b7975
dpaula <fernando.dpaula@gmail.com>
2018-06-16 21:04:31 -0300

    incluida validação do fformulario antes de enviar para o back end

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -2,14 +2,19 @@ angular.module('alurapic').controller('FotoController', function ($scope, $http)
 
     $scope.foto = {};
 
-    $scope.submeter = () => {//método chamado na tag do formulário da foto
-        $http.post("v1/fotos", $scope.foto) //envia foto para backend
-            .success(() => {
-                alert("Foto cadastrada com sucesso!");
-            })
-            .error((error) => {
-                console.log(error);
-            });
+    $scope.submeter = () => { //método chamado na tag do formulário da foto
+        if (!$scope.formulario.$valid) {//valida se o formulário está valido antes de mandar para o backend
+            return;
+        } else {
+            $http.post("v1/fotos", $scope.foto) //envia foto para backend
+                .success(() => {
+                    $scope.foto = {}; //lima o scopo
+                    alert("Foto cadastrada com sucesso!");
+                })
+                .error((error) => {
+                    console.log(error);
+                });
+        }
     };
 
 });
