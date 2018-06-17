4bbaca851f34a79085cb09ef9a35983d471c0b64
dpaula <fernando.dpaula@gmail.com>
2018-06-16 23:53:36 -0300

    criada nova diretiva para um botão onde é passada um método no ng-click

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -1,36 +1,51 @@
 angular.module('minhasDiretivas', [])
-.directive('meuPainel', () =>{
+    .directive('meuPainel', () => {
 
-    let ddo = {};
+        let ddo = {};
 
-    //definindo que a diretiva será usada como Atributo e Element
-    ddo.restrict = "AE";
+        //definindo que a diretiva será usada como Atributo e Element
+        ddo.restrict = "AE";
 
-    //escopo de atributos 
-    ddo.scope = {
-        titulo : '@' //interface de comunicação com a diretiva, como passa o dado para dentro do escopo da diretiva
-    };
+        //escopo de atributos 
+        ddo.scope = {
+            titulo: '@' //interface de comunicação com a diretiva, como passa o dado para dentro do escopo da diretiva
+        };
 
-    ddo.transclude = true; //para diretiva manter elementos filhos quando angular for processar o html
+        ddo.transclude = true; //para diretiva manter elementos filhos quando angular for processar o html
 
-    //apontando para o html com as diretivas novas
-    ddo.templateUrl = 'js/directives/meu-painel.html';
+        //apontando para o html com as diretivas novas
+        ddo.templateUrl = 'js/directives/meu-painel.html';
 
-    return ddo;
+        return ddo;
 
-})
-.directive('minhaFoto', () => {
+    })
+    .directive('minhaFoto', () => {
 
-    let ddo = {};
+        let ddo = {};
 
-    ddo.restrict = 'AE';
+        ddo.restrict = 'AE';
 
-    ddo.scope = {
-        url : '@',
-        titulo : '@'
-    };
+        ddo.scope = {
+            url: '@',
+            titulo: '@'
+        };
 
-    ddo.templateUrl = 'js/directives/minha-foto.html';
+        ddo.templateUrl = 'js/directives/minha-foto.html';
 
-    return ddo;
-});
+        return ddo;
+    })
+    .directive('meuBotaoRemover', () => {
+
+        let ddo = {};
+
+        ddo.restrict = 'E';
+
+        ddo.scope = {
+            nome: '@',
+            acao: '&'
+        };
+
+        ddo.template = '<button ng-click="acao(foto)" class="btn btn-danger btn-block">{{nome}}</button>';
+
+        return ddo;
+    });
