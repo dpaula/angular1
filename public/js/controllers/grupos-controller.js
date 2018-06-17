8ec9a00f4e4981ef85fcdda32b2e71e6664d2b2a
dpaula <fernando.dpaula@gmail.com>
2018-06-16 23:15:07 -0300

    novo controller para carregar os grupos, para o modulo alurapic

---
+++ public/js/controllers/grupos-controller.js
@@ -0,0 +1,12 @@
+angular.module('alurapic').controller('GruposController', function($scope, $http) {
+   
+    $scope.grupos = [];
+
+    $http.get('v1/grupos')
+    .success((grupos)=>{
+        $scope.grupos = grupos;
+    })
+    .error((error) =>{
+        console.log(error);
+    });
+});
